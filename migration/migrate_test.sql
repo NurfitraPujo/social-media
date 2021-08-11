a289e6ac818ba649529e5618ce2396578388d399
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-11 09:06:00 +0700

    Migration: added user table

--- migration/migrate_test.sql
+++ migration/migrate_test.sql
@@ -1,3 +1,12 @@
 CREATE SCHEMA IF NOT EXISTS social_media_test;
 
 GRANT ALL ON social_media_test.* TO 'ruby_dev'@'localhost';
+
+USE social_media_test;
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY(id)
+);
