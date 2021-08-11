72a005cf0f187cd978ce054fb954339bf9b460f1
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-11 17:29:43 +0700

    UserController #sign_up! should return error response
    #sign_up! does return hash of {status: 400, body: error_message} to indicate there is client specific error

--- controllers/user_controller.rb
+++ controllers/user_controller.rb
@@ -8,6 +8,9 @@ class UserController
   def sign_up!(request_data)
     user = @model.new(request_data)
     user.save
+  rescue UserError::UserInvalidError => e
+    { status: 400, body: e.message }
+  else
     { status: 201, body: '' }
   end
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -3,20 +3,40 @@ require './controllers/user_controller'
 
 describe UserController do
   describe '#sign_up!' do
-    context 'when called' do
+    context 'when user is new and valid' do
       it 'does register new user' do
         request_data = {
           username: 'fitra',
           email: 'fitra@gigih.com'
         }
         expected_response = {
-            status: 201,
-            body: ""
+          status: 201,
+          body: ''
         }
 
         model = double
         allow(model).to receive(:new).with(request_data).and_return(model)
         allow(model).to receive(:save)
+
+        user_co = UserController.new(model)
+        response = user_co.sign_up!(request_data)
+        expect(model).to have_received(:save)
+        expect(response).to eq(expected_response)
+      end
+    end
+    context 'when user is new but invalid' do
+      it 'does not register user, and return error response' do
+        request_data = {
+          username: 'fitra'
+        }
+        expected_response = {
+          status: 400,
+          body: 'Invalid or undefined required properties'
+        }
+
+        model = double
+        allow(model).to receive(:new).with(request_data).and_return(model)
+        allow(model).to receive(:save).and_raise(UserError::UserInvalidError)
 
         user_co = UserController.new(model)
         response = user_co.sign_up!(request_data)
