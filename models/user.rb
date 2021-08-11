3f4de7c0e9eb70deec89c591efe8a5d5af88a8c2
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-11 07:23:52 +0700

    Added #valid? method

--- models/user.rb
+++ models/user.rb
@@ -1,9 +1,16 @@
 class User
   attr_reader :username, :email, :bio
-  
+
   def initialize(user_data = {})
     @username = user_data[:username]
     @email = user_data[:email]
     @bio = user_data[:bio]
+  end
+
+  def valid?
+    return false if @username.nil?
+    return false if @email.nil?
+
+    true
   end
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -5,16 +5,29 @@ describe User do
   describe '#initialize' do
     context 'when initialized given arguments' do
       it 'should return User instance with given properties' do
-          user_data = {
-              username: 'fitra',
-              email: 'fitra@gigih.com',
-              bio: 'mengerjakan projek akhir'
-          }
-          user = User.new(user_data)
-          expect(user.username).to eq(user_data[:username])
-          expect(user.email).to eq(user_data[:email])
-          expect(user.bio).to eq(user_data[:bio])
-      end 
+        user_data = {
+          username: 'fitra',
+          email: 'fitra@gigih.com',
+          bio: 'mengerjakan projek akhir'
+        }
+        user = User.new(user_data)
+        expect(user.username).to eq(user_data[:username])
+        expect(user.email).to eq(user_data[:email])
+        expect(user.bio).to eq(user_data[:bio])
+      end
+    end
+  end
+
+  describe '#valid?' do
+    context 'when all required properties is given' do
+      it 'should return true' do
+        user_data = {
+          username: 'fitra',
+          email: 'fitra@gigih.com'
+        }
+        user = User.new(user_data)
+        expect(user.valid?).to eq(true)
+      end
     end
   end
 end
