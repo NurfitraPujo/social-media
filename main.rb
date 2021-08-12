81a14dafbfd7f50246f3da86f2550bdefa993180
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-12 07:17:44 +0700

    Main: Added UserRoutes to handle user routes

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require 'config'
 
-require './controllers/user_controller'
+require './routes/user_routes'
 
 class App < Sinatra::Application
   configure :development do
@@ -15,7 +15,7 @@ class App < Sinatra::Application
   set :bind, Settings.HOST
   set :port, Settings.PORT
 
-  use UserController
+  use UserRoutes
 
   run! if __FILE__ == $0
 end
