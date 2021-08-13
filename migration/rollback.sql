ac26f6a1762b6d9d177c07048c93fffbebd80ba9
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-13 08:22:49 +0700

    Fixed: Migrations rollback scheme, swapped the ordering of drop tables

--- migration/rollback.sql
+++ migration/rollback.sql
@@ -1,7 +1,7 @@
 USE social_media;
-DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS user;
 
 USE social_media_test;
-DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS user;
