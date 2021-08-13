e7d1ac37432dab557109a40ffcfcebb6f08b1f89
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-13 14:43:22 +0700

    Added POST handler for user who want to post

--- routes/user_routes.rb
+++ routes/user_routes.rb
@@ -1,12 +1,14 @@
 require 'sinatra/base'
 require 'sinatra/namespace'
 require './controllers/user_controller'
+require './controllers/post_controller'
 
 class UserRoutes < Sinatra::Base
   register Sinatra::Namespace
 
   configure do
-    set :controllers, UserController.new
+    set :user_controller, UserController.new
+    set :post_controller, PostController.new
   end
 
   namespace '/user' do
@@ -15,7 +17,15 @@ class UserRoutes < Sinatra::Base
         username: params['username'],
         email: params['email']
       }
-      settings.controllers.sign_up!(user_data)
+      settings.user_controller.sign_up!(user_data)
+    end
+    post '/:username/posts' do
+      post_data = {
+        username: params['username'],
+        text: params['text'],
+        timestamp: Time.now
+      }
+      settings.post_controller.post!(post_data)
     end
   end
 end
