c6724667dd33ac1009e9e21c53617fd008c5944d
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-15 07:15:40 +0700

    Hashtag #save will create new hashtag record if object is valid, and not otherwise

--- models/hashtag.rb
+++ models/hashtag.rb
@@ -4,12 +4,23 @@ class Hashtag
   def initialize(hashtag_data = {})
     @hashtag = hashtag_data[:hashtag]
     @occurence = 1
-    @timestamp = Time.now
+    @last_updated = Time.now
   end
 
   def valid?
     return false if @hashtag.nil? || @hashtag.strip.empty?
 
     true
+  end
+
+  def save(db_con = DatabaseConnection.instance)
+    return false unless valid?
+
+    db_con.query("INSERT INTO hashtag(hashtag, occurence, last_updated) VALUES ('#{@hashtag}', #{@occurence}, '#{@last_updated.strftime('%Y-%m-%d %H:%M:%S')}')")
+  end
+
+
+  def self.all(db_con = DatabaseConnection.instance)
+    db_con.query('SELECT * FROM hashtag')
   end
 end
--- spec/model/hashtag_spec.rb
+++ spec/model/hashtag_spec.rb
@@ -8,7 +8,7 @@ describe Hashtag do
     end
     it 'does define timestamp of when it was created' do
       hashtag = Hashtag.new(hashtag: 'testhashtag')
-      expect(hashtag.instance_variable_defined?(:@timestamp)).to eq(true)
+      expect(hashtag.instance_variable_defined?(:@last_updated)).to eq(true)
     end
   end
   describe '#valid?' do
@@ -22,6 +22,34 @@ describe Hashtag do
       it 'does return false' do
         hashtag = Hashtag.new
         expect(hashtag.valid?).to eq(false)
+      end
+    end
+  end
+  describe '#save' do
+    before(:each) do
+      db_con = DatabaseConnection.instance
+      db_con.query('DELETE FROM hashtag')
+    end
+    after(:all) do
+      db_con = DatabaseConnection.instance
+      db_con.query('DELETE FROM hashtag')
+    end
+    context 'when object is valid' do
+      context 'and when object is unique' do
+        it 'does create a new record on the persistence' do
+          hashtag = Hashtag.new(hashtag: 'testhashtag')
+          hashtag.save
+          hashtags = Hashtag.all
+          expect(hashtags.size).to eq(1)
+        end
+      end
+    end
+    context 'when object is invalid' do
+      it 'does not create a new record' do
+        hashtag = Hashtag.new
+        hashtag.save
+        hashtags = Hashtag.all
+        expect(hashtags.size).to be_zero
       end
     end
   end
