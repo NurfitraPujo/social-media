459d6c84e914b0a34796ee9eb1dc7777e03c05ca
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-18 18:44:28 +0700

    Refactor: Post .new does initialize new DateTime if timestamp is not provided

--- models/post.rb
+++ models/post.rb
@@ -16,14 +16,14 @@ class Post
   def initialize(post_data = {})
     @username = post_data[:username]
     @text = post_data[:text]
-    @timestamp = post_data[:timestamp]
+    @timestamp = post_data[:timestamp] || DateTime.now
   end
 
   def valid?
     return false if @username.nil?
     return false if @text.nil?
     return false if text_is_empty?(@text)
-    return false if @timestamp.nil?
+    return false if @timestamp.nil? 
 
     true
   end
