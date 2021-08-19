d61b269f47cc7d7d1b0a2961ae2dda9c219d7263
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-08-19 14:56:18 +0700

    Routes: Added PostRoutes to handle post related request

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader'
 require 'config'
 
 require './routes/user_routes'
+require './routes/post_routes'
 
 class App < Sinatra::Application
   configure :development do
@@ -15,7 +16,9 @@ class App < Sinatra::Application
   set :bind, Settings.HOST
   set :port, Settings.PORT
 
+
   use UserRoutes
+  use PostRoutes
 
   run! if __FILE__ == $0
 end
